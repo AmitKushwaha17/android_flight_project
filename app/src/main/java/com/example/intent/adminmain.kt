30e467ea1fd0a407e48f471da29b96986ee296ec
admin <nathuneekushwaha@gmail.com>
2024-09-18 09:01:58 +0530

    ty

--- app/src/main/java/com/example/intent/adminmain.kt
+++ app/src/main/java/com/example/intent/adminmain.kt
@@ -11,20 +11,20 @@ class adminmain : AppCompatActivity() {
         super.onCreate(savedInstanceState)
 
         setContentView(R.layout.activity_adminmain)
-        val userimage = findViewById<ImageView>(R.id.imageView)
+      //  val userimage = findViewById<ImageView>(R.id.imageView)
         val flightimage = findViewById<ImageView>(R.id.imageView2)
         val logoutimage = findViewById<ImageView>(R.id.imageView3)
-        val usertxt = findViewById<TextView>(R.id.Usermanage)
+        //val usertxt = findViewById<TextView>(R.id.Usermanage)
         val flighttxt = findViewById<TextView>(R.id.flightmanage)
         val logouttxt = findViewById<TextView>(R.id.logout)
-        userimage.setOnClickListener {
-            intent= Intent(this,usermanage::class.java)
-            startActivity(intent)
-        }
-        usertxt.setOnClickListener{
-            intent=Intent(this,usermanage::class.java)
-            startActivity(intent)
-        }
+       // userimage.setOnClickListener {
+         //   intent= Intent(this,usermanage::class.java)
+           // startActivity(intent)
+        //}
+//        usertxt.setOnClickListener{
+//            intent=Intent(this,usermanage::class.java)
+//            startActivity(intent)
+//        }
         flightimage.setOnClickListener {
             intent=Intent(this,flightmanage::class.java)
             startActivity(intent)
